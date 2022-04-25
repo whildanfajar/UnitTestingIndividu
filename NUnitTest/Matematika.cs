b7cd31ff47fcb84b791492bf878bd3896e057f2c
whildan fajar <whildanfajarhidayat93@gmail.com>
2022-04-25 21:51:10 +0700

    Add project files.

---
+++ NUnitTest/Matematika.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest
+{
+    class Matematika
+    {
+        public string User(string name)
+        {
+            return name;
+        }
+
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
+
---
+++ NUnitTest/TestCase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    class TestCase
+    {
+        [TestCase]
+        public void User()
+        {
+            Matematika mtk = new Matematika();
+            Assert.AreEqual("WhildanFajar", mtk.User("Whildan"));
+        }
+
+        [TestCase]
+        public void Tambah()
+        {
+            Matematika mtk = new Matematika();
+            Assert.AreEqual(31, mtk.Tambah(20, 11));
+        }
+
+        [TestCase]
+        public void Kurang()
+        {
+            Matematika mtk = new Matematika();
+            Assert.AreEqual(9, mtk.Kurang(20, 11));
+        }
+    }
+}
